2ee629773c0707a393e99d05d887c0f8b54eb5a5
Andrew Joseph Baumann <baumann.andrew14@gmail.com>
2015-05-29 19:41:32 -0400

    bubble sort completed

--- src/Sorts.java
+++ src/Sorts.java
@@ -4,14 +4,17 @@ import java.util.List;
 public class Sorts {
 
     private int[] rande = new int[20];
+    private int size = 0;
 
     public Sorts(int[] d, int size)
     {
         rande = d;
-        my_sele(size);
+        this.size = size;
+        //my_sele();
+        my_bubble();
     }
 
-    public void my_sele(int size)
+    public void my_sele()
     {
         System.out.println("Results of selection sort:");
         int temp = 0;
@@ -44,6 +47,27 @@ public class Sorts {
 
     public void my_bubble()
     {
+        System.out.println("Results of bubble sort:");
+        int temp = 0;
+        int temp_pos = 0;
+        int[] brand = rande;
+
+        for(int i=0; i<size; i++)
+        {
+            temp = brand[i];
+            temp_pos = i;
+            for(int x=i+1; x<size; x++)
+            {
+                if(brand[x] < temp)
+                {
+                    temp = brand[x];
+                    brand[x] = brand[i];
+                    brand[i] = temp;
+                }
+            }
+        }
+        for(int i=0; i<size; i++)
+            System.out.println(brand[i]);
     }
 
     public void my_merge()
